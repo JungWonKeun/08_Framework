9a1facd2db30f44c924b8637d6ecc1868dc759d2
yukpoyum <anokata@naver.com>
2024-09-10 17:18:31 +0900

    20240910_Framework_Ajax_todolist

--- TodoList-Ajax/src/main/java/edu/kh/todolist/service/TodoListServiceImpl.java
+++ TodoList-Ajax/src/main/java/edu/kh/todolist/service/TodoListServiceImpl.java
@@ -57,6 +57,7 @@ public class TodoListServiceImpl implements TodoListService{
 		return mapper.todoComplete(todoNo);
 	}
 	
+	
 	// 할 일 수정
 	@Override
 	public int todoUpdate(Todo todo) {
@@ -73,6 +74,25 @@ public class TodoListServiceImpl implements TodoListService{
 	@Override
 	public String searchTitle(int todoNo) {
 		return mapper.searchTitle(todoNo);
+	}
+	
+	
+	// 전체 할 일 개수 조회
+	@Override
+	public int getTotalCount() {
+		return mapper.getTotalCount();
+	}
+	
+	// 완료된 할 일 개수 조회
+	@Override
+	public int getCompleteCount() {
+		return mapper.selectCompleteCount();
+	}
+	
+	// 할 일 전체 목록
+	@Override
+	public List<Todo> getTodoList() {
+		return mapper.selectTodoList();
 	}
 }
 
